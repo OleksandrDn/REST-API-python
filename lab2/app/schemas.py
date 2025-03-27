6fef013bba981c7e3d714623351a5d3ff8434be3
OleksandrDn <150485707+OleksandrDn@users.noreply.github.com>
2025-03-27 16:59:39 +0200

    Update schemas.py

--- lab2/app/schemas.py
+++ lab2/app/schemas.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class Book(BaseModel):
-    id: Optional[int] = None  # ID тепер необов'язковий
+    id: Optional[int] = None  
     title: str = Field(..., min_length=1)
     author: str = Field(..., min_length=1)
     year: int = Field(..., gt=0)
